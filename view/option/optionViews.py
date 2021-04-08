2816b207b7ef16a998ddc71fb55fe55c47106626
haru1843 <haru1843inf@gmail.com>
2021-04-08 18:53:21 +0900

    update: views inherit from 'base'

--- view/option/optionViews.py
+++ view/option/optionViews.py
@@ -1,78 +1,100 @@
-from PyQt5.QtWidgets import QWidget, QPushButton
+from PyQt5.QtWidgets import QWidget, QPushButton, QRadioButton, QLabel
 from PyQt5.QtCore import QCoreApplication
 
+from ..base import BaseWidget
 
-class Target(QWidget):
+
+class Target(BaseWidget):
     def __init__(self, parent):
-        super().__init__(parent)
+        super().__init__(parent, title="選択方法の設定")
         self.master = parent
 
-        # self.button1 = QPushButton('Option', self)
-        # self.button1.move(230, 120)
-        # self.button1.clicked.connect(self.welcome)
+        self.rbtn_files = QRadioButton("ファイル単位", self)
+        self.rbtn_files.move(175, 180)
+        self.rbtn_folders = QRadioButton("フォルダー単位", self)
+        self.rbtn_folders.move(175, 310)
 
-        # self.quitbutton1 = QPushButton('exit', self)
-        # self.quitbutton1.move(230, 200)
-        # self.quitbutton1.clicked.connect(QCoreApplication.instance().quit)
+        self.rbtn_folders.setChecked(True)
 
-    def welcome(self):
+    def nextPage(self):
+        if self.rbtn_files.isChecked():
+            print(self.rbtn_files.text())
+
+        if self.rbtn_folders.isChecked():
+            print(self.rbtn_folders.text())
+
         self.master.setCurrentIndex(
-            self.master.tab_index_dict["process"]
+            self.master.currentIndex() + 1
         )
 
 
-class Runtime(QWidget):
+class Runtime(BaseWidget):
     def __init__(self, parent):
-        super().__init__(parent)
+        super().__init__(parent, title="実行時の設定")
         self.master = parent
 
-        # self.button1 = QPushButton('Option', self)
-        # self.button1.move(230, 120)
-        # self.button1.clicked.connect(self.welcome)
+        self.rbtn_files = QRadioButton("a", self)
+        self.rbtn_files.move(175, 180)
+        self.rbtn_folders = QRadioButton("b", self)
+        self.rbtn_folders.move(175, 310)
 
-        # self.quitbutton1 = QPushButton('exit', self)
-        # self.quitbutton1.move(230, 200)
-        # self.quitbutton1.clicked.connect(QCoreApplication.instance().quit)
+        self.rbtn_folders.setChecked(True)
 
-    def welcome(self):
+    def nextPage(self):
+        if self.rbtn_files.isChecked():
+            print(self.rbtn_files.text())
+
+        if self.rbtn_folders.isChecked():
+            print(self.rbtn_folders.text())
+
         self.master.setCurrentIndex(
-            self.master.tab_index_dict["process"]
+            self.master.currentIndex() + 1
         )
 
 
-class Result(QWidget):
+class Result(BaseWidget):
     def __init__(self, parent):
-        super().__init__(parent)
+        super().__init__(parent, title="結果の表示設定")
         self.master = parent
 
-        # self.button1 = QPushButton('Option', self)
-        # self.button1.move(230, 120)
-        # self.button1.clicked.connect(self.welcome)
+        self.rbtn_files = QRadioButton("a", self)
+        self.rbtn_files.move(175, 180)
+        self.rbtn_folders = QRadioButton("b", self)
+        self.rbtn_folders.move(175, 310)
 
-        # self.quitbutton1 = QPushButton('exit', self)
-        # self.quitbutton1.move(230, 200)
-        # self.quitbutton1.clicked.connect(QCoreApplication.instance().quit)
+        self.rbtn_folders.setChecked(True)
 
-    def welcome(self):
+    def nextPage(self):
+        if self.rbtn_files.isChecked():
+            print(self.rbtn_files.text())
+
+        if self.rbtn_folders.isChecked():
+            print(self.rbtn_folders.text())
+
         self.master.setCurrentIndex(
-            self.master.tab_index_dict["process"]
+            self.master.currentIndex() + 1
         )
 
 
-class Log(QWidget):
+class Log(BaseWidget):
     def __init__(self, parent):
-        super().__init__(parent)
+        super().__init__(parent, title="ログの出力設定")
         self.master = parent
 
-        # self.button1 = QPushButton('Option', self)
-        # self.button1.move(230, 120)
-        # self.button1.clicked.connect(self.welcome)
+        self.rbtn_files = QRadioButton("a", self)
+        self.rbtn_files.move(175, 180)
+        self.rbtn_folders = QRadioButton("b", self)
+        self.rbtn_folders.move(175, 310)
 
-        # self.quitbutton1 = QPushButton('exit', self)
-        # self.quitbutton1.move(230, 200)
-        # self.quitbutton1.clicked.connect(QCoreApplication.instance().quit)
+        self.rbtn_folders.setChecked(True)
 
-    def welcome(self):
+    def nextPage(self):
+        if self.rbtn_files.isChecked():
+            print(self.rbtn_files.text())
+
+        if self.rbtn_folders.isChecked():
+            print(self.rbtn_folders.text())
+
         self.master.setCurrentIndex(
-            self.master.tab_index_dict["process"]
+            self.master.currentIndex() + 1
         )
--- view/viewController.py
+++ view/viewController.py
@@ -1,34 +1,52 @@
-from PyQt5.QtWidgets import QTabWidget
+from PyQt5.QtWidgets import QTabWidget, QPushButton
+from PyQt5.QtCore import QCoreApplication
 
-from .option.optionView import OptionView
+# from .option.optionViews import Target, Runtime, Result, Log
+from .option import optionViews
 from .process.processView import ProcessView
 
 
 class App(QTabWidget):
     def __init__(self):
         super().__init__()
+
         self.setWindowTitle("Hello")
 
+        # self.quitbutton1 = QPushButton('exit', self)
+        # self.quitbutton1.move(230, 200)
+        # self.quitbutton1.clicked.connect(QCoreApplication.instance().quit)
+
         # 各ページのインスタンス化
-        self.tab1 = OptionView(self)
-        self.tab2 = ProcessView(self)
+        self.tab1 = optionViews.Target(self)
+        self.tab2 = optionViews.Runtime(self)
+        self.tab3 = optionViews.Result(self)
+        self.tab4 = optionViews.Log(self)
 
         # タブに各ページを追加
-        self.addTab(self.tab1, "StartMenu")
-        self.addTab(self.tab2, "WelcomeMenu")
+        self.addTab(self.tab1, "option-target")
+        self.addTab(self.tab2, "option-runtime")
+        self.addTab(self.tab3, "option-result")
+        self.addTab(self.tab4, "option-log")
 
         # 各ページとインデックスの対応表
-        self.tabIndexDict = {
+        self.tab_index_dict = {
             "option": 0,
             "process": 1,
         }
 
         # タブパネルのボーダーを削除
         self.setStyleSheet("QTabWidget::pane { border: 0; }")
+
+        # self.master_next_button.setStyleSheet("QPushButton { background-color: white }")
+
         # タブバーを非表示に(↓をコメントすると動きがわかりやすくなるかも)
         self.tabBar().hide()
-        self.resize(500, 300)
+        self.resize(960, 540)
         self.move(100, 0)
+
+    def master_next_page(self):
+        print(self.currentIndex())
+        self.setCurrentIndex((self.currentIndex()+1) % len(self.tab_index_dict))
 
     def piyo_piyo(self, text):
         print(text)

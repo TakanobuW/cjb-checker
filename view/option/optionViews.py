d29fe710387e7e5be2a99de7e14942a67453e2dc
haru1843 <haru1843inf@gmail.com>
2021-04-08 19:37:35 +0900

    update: simple option-runtime view

--- view/option/optionViews.py
+++ view/option/optionViews.py
@@ -24,7 +24,8 @@ class Target(BaseWidget):
             self.master.option["target"] = "folders"
             # print(self.rbtn_folders.text())
         else:
-            print("わぁ")
+            print("Unecpected behavior in option-target")
+            self.master.option["target"] = "folders"
 
         self.master.setCurrentIndex(
             self.master.currentIndex() + 1
@@ -36,19 +37,22 @@ class Runtime(BaseWidget):
         super().__init__(parent, title="実行時の設定")
         self.master = parent
 
-        self.rbtn_files = QRadioButton("a", self)
-        self.rbtn_files.move(175, 180)
-        self.rbtn_folders = QRadioButton("b", self)
-        self.rbtn_folders.move(175, 310)
+        self.rbtn_speed = QRadioButton("速度重視", self)
+        self.rbtn_speed.move(175, 180)
+        self.rbtn_accurate = QRadioButton("正確さ重視", self)
+        self.rbtn_accurate.move(175, 310)
 
-        self.rbtn_folders.setChecked(True)
+        self.rbtn_accurate.setChecked(True)
 
     def nextPage(self):
-        if self.rbtn_files.isChecked():
-            print(self.rbtn_files.text())
 
-        if self.rbtn_folders.isChecked():
-            print(self.rbtn_folders.text())
+        if self.rbtn_speed.isChecked():
+            self.master.option["runtime"] = "speed"
+        elif self.rbtn_accurate.isChecked():
+            self.master.option["runtime"] = "accurate"
+        else:
+            print("Unecpected behavior in option-runtime")
+            self.master.option["runtime"] = "accurate"
 
         self.master.setCurrentIndex(
             self.master.currentIndex() + 1

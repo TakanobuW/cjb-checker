1675f28b137e6e15c996a989da15e870f5d4d3f3
haru1843 <haru1843inf@gmail.com>
2021-04-10 00:05:57 +0900

    add: table view

--- view/process/processView.py
+++ view/process/processView.py
@@ -1,7 +1,9 @@
-from PyQt5.QtWidgets import QWidget, QPushButton
+from PyQt5.QtWidgets import QWidget, QPushButton, QTableWidget, QTableWidgetItem
 from PyQt5.QtCore import QCoreApplication
 
 from ..base import BaseWidget
+
+from controller.fileChecker import FileChecker
 
 
 class FileCheckProcessView(BaseWidget):
@@ -9,14 +11,40 @@ class FileCheckProcessView(BaseWidget):
         super().__init__(parent, title="ファイル内容の確認")
         self.master = parent
 
-        self.add_button = QPushButton('実行', self)
-        self.add_button.move(450, 430)
-        self.add_button.clicked.connect(self.execute_check)
+        self.run_button = QPushButton('実行', self)
+        self.run_button.move(450, 490)
+        self.run_button.clicked.connect(self.executeCheck)
 
-        self.hideNextButton()
+        self.table = QTableWidget(self)
+        self.table.hide()
 
-    def execute_check(self):
-        print(self.master.file_path_list)
+        self.disableNextButton()
+
+    def executeCheck(self):
+        self.run_button.hide()
+        checker = FileChecker()
+
+        for nth, path in enumerate(self.master.file_path_list):
+            # print(f"{nth+1} / {len(self.master.file_path_list)}")
+            checker.readFile(file_path=path)
+
+        self.master.file_check_result = checker.getResult()
+        self.updateTable()
+        self.table.move(50, 70)
+        self.table.resize(860, 400)
+        self.table.setSortingEnabled(True)
+        self.table.show()
+
+        self.enableNextButton()
+
+    def updateTable(self):
+        keys = list(self.master.file_check_result[0].keys())
+        self.table.setRowCount(len(self.master.file_check_result))
+        self.table.setColumnCount(len(keys))
+        self.table.setHorizontalHeaderLabels(keys)
+        for nth, result in enumerate(self.master.file_check_result):
+            for key_idx, key in enumerate(keys):
+                self.table.setItem(nth, key_idx, QTableWidgetItem(str(result[key])))
 
     def nextPage(self):
         if self.master.option["check"]["run"]:
@@ -34,13 +62,13 @@ class RunCheckProcessView(BaseWidget):
         super().__init__(parent, title="回路の動作確認中...")
         self.master = parent
 
-        self.add_button = QPushButton('実行', self)
-        self.add_button.move(450, 430)
-        self.add_button.clicked.connect(self.execute_check)
+        self.run_button = QPushButton('実行', self)
+        self.run_button.move(450, 430)
+        self.run_button.clicked.connect(self.executeCheck)
 
         self.hideNextButton()
 
-    def execute_check(self):
+    def executeCheck(self):
         print(self.master.file_path_list)
 
         if self.master.option["check"]["run-target"] == "work1":

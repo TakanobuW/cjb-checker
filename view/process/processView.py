0ebd4af3ba1631f9a1fd93dec5a78570009c3e5d
haru1843 <haru1843inf@gmail.com>
2021-04-09 17:48:48 +0900

    update: divide process for 'fileCheck' and 'runCheck'

--- view/process/processView.py
+++ view/process/processView.py
@@ -2,19 +2,13 @@ from PyQt5.QtWidgets import QWidget, QPushButton
 from PyQt5.QtCore import QCoreApplication
 
 
-class ProcessView(QWidget):
+class FileCheckProcessView(QWidget):
     def __init__(self, parent):
         super().__init__(parent)
         self.master = parent
 
-        # self.button1 = QPushButton('process!', self)
-        # self.button1.move(230, 120)
-        # self.button1.clicked.connect(self.welcome)
 
-        # self.quitbutton1 = QPushButton('exit', self)
-        # self.quitbutton1.move(230, 200)
-        # self.quitbutton1.clicked.connect(QCoreApplication.instance().quit)
-
-    def welcome(self):
-        # 0オリジン
-        self.master.setCurrentIndex(0)
+class RunCheckProcessView(QWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.master = parent

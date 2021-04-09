b7d54e2f4c3e3edd62bc07a97ceb7687364f6d68
haru1843 <haru1843inf@gmail.com>
2021-04-09 17:18:21 +0900

    add: selectedList widget

--- view/select/selectViews.py
+++ view/select/selectViews.py
@@ -1,7 +1,51 @@
-from PyQt5.QtWidgets import QWidget, QPushButton, QRadioButton, QLabel, QCheckBox
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtWidgets import (
+    QWidget,
+    QPushButton,
+    QRadioButton,
+    QLabel,
+    QFileDialog,
+    QScrollArea,
+    QVBoxLayout,
+    QListWidget
+)
+from PyQt5.QtCore import QCoreApplication, Qt
+from typing import List, Set
 
 from ..base import BaseWidget
+
+
+class SelectedList(QListWidget):
+    def __init__(self, parent):
+        super().__init__(parent=parent)
+
+        self._path_list = set([])
+        self._updateView()
+
+        self.itemClicked.connect(self._removePath)
+
+    def addPath(self, path: str):
+        self._path_list.add(path)
+        self._updateView()
+
+    def _updateView(self):
+        self.clear()
+        self.addItems(self._path_list)
+
+    def clearPath(self):
+        self._path_list = set([])
+        self._updateView()
+
+    def _removePath(self, target_item):
+        target_idx = self.row(target_item)
+
+        _ = self.takeItem(target_idx)
+        # item = None
+
+        self._path_list.discard(target_item.text())
+        self._updateView()
+
+    def getPathList(self) -> Set[str]:
+        return self._path_list
 
 
 class FileSelect(BaseWidget):
@@ -37,12 +81,18 @@ class FolderSelect(BaseWidget):
         super().__init__(parent, title="フォルダの選択")
         self.master = parent
 
-        self.rbtn_files = QRadioButton("ファイル単位", self)
-        self.rbtn_files.move(175, 180)
-        self.rbtn_folders = QRadioButton("フォルダー単位", self)
-        self.rbtn_folders.move(175, 310)
+        self.add_button = QPushButton('対象フォルダを追加', self)
+        self.add_button.move(450, 430)
+        self.add_button.clicked.connect(self.showFileDialog)
 
-        self.rbtn_folders.setChecked(True)
+        self.selected_list = SelectedList(self)
+        self.selected_list.resize(860, 300)
+        self.selected_list.move(50, 100)
+
+    def showFileDialog(self):
+        # 第二引数はダイアログのタイトル、第三引数は表示するパス
+        path = str(QFileDialog.getExistingDirectory(self, '対象フォルダの選択', '/home'))
+        self.selected_list.addPath(path)
 
     def nextPage(self):
         if self.rbtn_files.isChecked():
